7442ca321a69cccff7cb09a5c9da20f908853329
Janik77 <61647597+Janik77@users.noreply.github.com>
2025-12-24 22:21:54 +0500

    Add seed_demo management command

--- core/views.py
+++ core/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from .forms import LeadRequestForm
+from .models import LeadRequest
+
+
+class LeadRequestCreateView(CreateView):
+    model = LeadRequest
+    form_class = LeadRequestForm
+    success_url = reverse_lazy("pages:contacts")
+
+    def form_valid(self, form):
+        form.instance.page = self.request.path
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return f"{self.success_url}?success=1"
--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+from core.views import LeadRequestCreateView
+
+
+class HomeView(LeadRequestCreateView):
+    template_name = "pages/home.html"
+    success_url = "/?success=1"
+
+
+class AboutView(TemplateView):
+    template_name = "pages/about.html"
+
+
+class ServicesView(TemplateView):
+    template_name = "pages/services.html"
+
+
+class ContactsView(LeadRequestCreateView):
+    template_name = "pages/contacts.html"
